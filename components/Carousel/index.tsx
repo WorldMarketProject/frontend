ee76aff944144377ca1dea5d3fb74eb681bbbfc6
ilimes <ilimes1307@gmail.com>
2024-02-04 00:20:33 +0900

    Fix: NoSSR으로 인해 이미지 뜨지 않는 문제 수정

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -3,7 +3,6 @@
 import { useState, useEffect } from 'react';
 import { Col, Row, Spin, Image as AntImage } from 'antd';
 import Image from 'next/image';
-import NoSSr from '../NoSsr';
 import styled from 'styled-components';
 import * as S from "./style";
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
@@ -41,7 +40,6 @@ const CarouselComponent = () => {
       <div>
         <Row>
           <Col span={24} style={{ padding: '0 25px' }}>
-            <NoSSr>
               <S.StyledCarousel
                 arrows
                 dotPosition={"bottom"}
@@ -64,21 +62,10 @@ const CarouselComponent = () => {
                 {
                   contents?.map((e, i) =>
                     <div key={i}>
-                      <Row
-                        style={{
-                          maxWidth: 1200,
-                          margin: "0 auto",
-                          cursor: "pointer",
-                        }}
-                      >
-                        <Col span={24} style={{ overflow: "hidden" }}>
-                          <AntImage preview={{ mask: false }} src={e?.img} />
-                        </Col>
-                      </Row>
+                      <AntImage preview={{ mask: false }} src={e?.img} style={{ cursor: 'pointer' }} />
                     </div>)
                 }
               </S.StyledCarousel>
-            </NoSSr>
           </Col>
         </Row>
       </div>
