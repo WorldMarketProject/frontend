972a7bbdcffc31b60d6a5025a5028a5b7e635dc9
LIME <95404736+ilimes@users.noreply.github.com>
2024-03-06 23:23:03 +0900

    Fix: 계정 관리 - 내 정보 수정

--- components/MyPage/Account/index.tsx
+++ components/MyPage/Account/index.tsx
@@ -1,38 +1,86 @@
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import * as S from './style';
 import { getUserInfo } from '@/api/user/api';
+
+const InputLabel = ({
+  userInfo,
+  editInfo,
+  setEditInfo,
+  isEdit,
+  name,
+}: {
+  userInfo: any;
+  editInfo: any;
+  setEditInfo: any;
+  isEdit: boolean;
+  name: string;
+}) =>
+  isEdit ? (
+    <Input
+      value={editInfo?.[name]}
+      onChange={(e) => setEditInfo({ ...editInfo, [name]: e.target.value })}
+    />
+  ) : (
+    <span>{userInfo?.[name]}</span>
+  );
+
+const infoList = [
+  { title: '이름', key: 'user_nm' },
+  { title: '닉네임', key: 'user_nick' },
+  { title: '아이디', key: 'user_id' },
+  { title: '상태', key: 'user_s_nm' },
+  { title: '등급', key: 'user_l_nm' },
+  { title: '생년월일', key: 'user_birth' },
+  { title: '모바일 인증 여부', key: 'mobile_cert_yn' },
+  { title: '가입일', key: 'reg_dt' },
+];
 
 const Account = () => {
   const { data, isSuccess, isError, isLoading } = useQuery({
     queryKey: ['userInfo'],
     queryFn: getUserInfo,
   });
-  const userInfo = data?.info;
+  const userInfo = data?.info ?? {};
+
+  const [isEdit, setIsEdit] = useState(false);
+  const [editInfo, setEditInfo] = useState({});
+
+  const btnLabel = isEdit ? '수정 완료' : '회원 정보 수정';
+
+  useEffect(() => {
+    if (Object.keys(userInfo)?.length !== 0) {
+      setEditInfo(userInfo);
+    }
+  }, [userInfo]);
 
   return (
     <>
       <S.SubTitle>내 정보</S.SubTitle>
       <S.StyledBoxDiv style={{ paddingBottom: 26 }}>
-        <h3>이름</h3>
-        <div>{userInfo?.user_nm}</div>
-        <h3>닉네임</h3>
-        <div>{userInfo?.user_nick}</div>
-        <h3>아이디</h3>
-        <div>{userInfo?.user_id}</div>
-        <h3>상태</h3>
-        <div>{userInfo?.user_s_nm}</div>
-        <h3>등급</h3>
-        <div>{userInfo?.user_l_nm}</div>
-        <h3>생년월일</h3>
-        <div>{userInfo?.user_birth || '없음'}</div>
-        <h3>모바일 인증 여부</h3>
-        <div>{userInfo?.mobile_cert_yn}</div>
-        <h3>가입일</h3>
-        <div>{userInfo?.reg_dt}</div>
+        {infoList.map((e: any, i: number) => (
+          <div key={e.key}>
+            <h3>{e.title}</h3>
+            <div>
+              <InputLabel
+                userInfo={userInfo}
+                editInfo={editInfo}
+                setEditInfo={setEditInfo}
+                isEdit={isEdit}
+                name={e.key}
+              />
+            </div>
+          </div>
+        ))}
         <div style={{ textAlign: 'right', marginTop: 16 }}>
-          <Button type="primary" size="large" style={{ fontWeight: 600 }}>
-            회원 정보 수정
+          <Button
+            type="primary"
+            size="large"
+            onClick={() => setIsEdit(!isEdit)}
+            style={{ fontWeight: 600 }}
+          >
+            {btnLabel}
           </Button>
         </div>
       </S.StyledBoxDiv>
