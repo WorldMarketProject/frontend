5779d30f3b66379a749e33634420234f60c40c14
LIME <95404736+ilimes@users.noreply.github.com>
2024-04-03 22:09:37 +0900

    Fix: https ssl 적용

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,73 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* css-in-js FOUC 문제 해결 */
-    compiler: {
-        styledComponents: true,
-    },
-    typescript: {
-        ignoreBuildErrors: false,
-    },
-    /* useEffect 2번 실행되는 문제 해결 */
-    reactStrictMode: false,
-    webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ["@svgr/webpack"],
-        });
 
-        return config;
-    },
-    images: {
-        domains: [
-            "img.freepik.com",
-            "images.pexels.com"
-        ],
-    },
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: 'upgrade-insecure-requests',
+    // value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
+
+const nextConfig = {
+  /* css-in-js FOUC 문제 해결 */
+  compiler: {
+    styledComponents: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  /* useEffect 2번 실행되는 문제 해결 */
+  reactStrictMode: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  images: {
+    domains: ['img.freepik.com', 'images.pexels.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
